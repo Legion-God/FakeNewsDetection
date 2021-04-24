6f7a6d32e5983559064c17288071da4d7e9dca9c
Shubhendra Rajkarne <srajkarne9@gmail.com>
2021-04-24 16:52:46 +0530

    done with flask and env setup

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from flask import (Flask,
+                   render_template)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/result')
+def results():
+    return render_template('results.html', fnews_web_result_content="<h2>This is for testing purpose, "
+                                                                    "you can pass results here</h2>")
